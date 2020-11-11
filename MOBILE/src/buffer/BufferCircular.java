b58bccde7782dea839a45167605991c3ac7b3603
Diego <Diego@Diego-PC>
2020-11-11 19:49:07 -0300

    buffer sincronizado/circular

---
+++ MOBILE/src/buffer/BufferCircular.java
@@ -0,0 +1,43 @@
+package buffer;
+
+import java.util.LinkedList;
+
+public class BufferCircular implements IBuffer {
+	
+	private LinkedList<Integer> buffer = new LinkedList<>();
+	private static int MaxSize = 3;
+	
+	public static void setBufferSize(int tamanho) {
+		MaxSize = tamanho;
+	}
+
+	@Override
+	public synchronized void escrever(Produtor Escreve, int valor) throws InterruptedException {
+		while (this.buffer.size() == MaxSize) {
+			System.out.println("*Buffer cheio*");
+			wait();
+		}
+		
+		buffer.add(valor);
+		System.out.println(String.format("\t\"%s\" escreveu o valor %s", Escreve.getNome(), valor));
+		
+		notifyAll();		
+	}
+
+	@Override
+	public synchronized int ler(Consumidor Le) throws InterruptedException {
+		while(this.buffer.size() == 0) {
+			System.out.println("*Buffer vazio*");
+			wait();
+		}
+		
+		int valor = this.buffer.removeFirst();
+		System.out.println(String.format("\t\t\"%s\" leu o valor %s", Le.getNome(), valor));
+		
+		notifyAll();
+		
+		return valor;
+		
+	}
+
+}
---
+++ MOBILE/src/buffer/Consumidor.java
@@ -0,0 +1,18 @@
+package buffer;
+
+public class Consumidor extends Entidade {
+	
+	public Consumidor(String nome, IBuffer bufferShared) {
+		super(nome, bufferShared);
+	}
+
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			try {
+				this.bufferShared.ler(this);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}	
+}
